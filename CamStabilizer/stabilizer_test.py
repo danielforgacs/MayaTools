8c04bce2d6b81afebbb01d1b77b45f79c7693d6c
forgacs.daniel <forgacs.daniel@gmail.com>
2016-01-22 10:44:51 +0000

    updated cam stabilizer unit & functional test

--- CamStabilizer/stabilizer_test.py
+++ CamStabilizer/stabilizer_test.py
@@ -2,18 +2,22 @@
 """
 
 import unittest
+import time
+import pymel.core
+# import pymel.core.uitypes
+import stabilizer
+reload(stabilizer)
 
 
 class TestSceneSetup(unittest.TestCase):
     def setUp(self):
-        import pymel.core
 
         pymel.core.newFile(force=True)
 
         pymel.core.polyCube()
 
         camaim = pymel.core.spaceLocator()
-        cam, camshape = pymel.core.camera(displayResolution=True, displayFilmGate=True, overscan=1.8)
+        cam, camshape = pymel.core.camera(displayResolution=True, displayFilmGate=False, overscan=1.8)
 
         cam.setAttr('translateZ', 5)
         cam.setParent(camaim)
@@ -29,15 +33,44 @@ class TestSceneSetup(unittest.TestCase):
         pymel.core.setKeyframe(camaim)
         pymel.core.lookThru('perspView', camshape)
 
+        pymel.core.select('pCube1.vtx[5]')
+
+    def tearDown(self):
+        window = pymel.core.uitypes.Window('Stabilizer')
+
+        try:
+            window.delete()
+        except:
+            pass
+
 
 class StabizilerTests(TestSceneSetup):
     def test_unittests_running(self):
         self.assertTrue(True)
 
+    def test_gui_creates_stabilizer_window(self):
+        stabilizer.gui()
+        self.assertTrue(pymel.core.window('Stabilizer', query=True, exists=True))
+
+    def test_window_has_stabilize_button(self):
+        stabilizer.gui()
+        self.assertTrue(pymel.core.uitypes.Button('button_stabilizer'))
+
+    def test_stabilizer_button_command_is_stabilize(self):
+        stabilizer.gui()
+        button = pymel.core.uitypes.Button('button_stabilizer')
+
+        self.assertEqual(button.getCommand(), 'fstab.stabilizer("start")')
+
+
 
 class StabilizerFunctionalTests(TestSceneSetup):
     def test_functional_tests_running(self):
         self.assertTrue(True)
+
+    def test_functional(self):
+        stabilizer.main()
+        self.assertTrue(pymel.core.window('Stabilizer', query=True, exists=True))
 
 
 
