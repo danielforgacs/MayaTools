861ddeb44f583eeee2a890df4061d277f27fcf24
forgacs.daniel <forgacs.daniel@gmail.com>
2016-01-22 00:26:16 +0000

    stabilizer test update

--- CamStabilizer/stabilizer_test.py
+++ CamStabilizer/stabilizer_test.py
@@ -1,4 +1,15 @@
-import sys
+import unittest
 
-print (sys.path)
-# import cam_stabilizer
+
+class StabizilerTests(unittest.TestCase):
+    def test_unittests_running(self):
+        self.assertTrue(True)
+
+
+class StabilizerFunctionalTests(unittest.TestCase):
+    def test_functional_tests_running(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=1)
