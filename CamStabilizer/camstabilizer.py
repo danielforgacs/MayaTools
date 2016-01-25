307e944083372941e0b668bf5942cc723f22978a
forgacs.daniel <forgacs.daniel@gmail.com>
2016-01-25 21:32:05 +0000

    cam stab update

--- CamStabilizer/camstabilizer.py
+++ CamStabilizer/camstabilizer.py
@@ -1,9 +1,31 @@
+import logging
+
 import pymel.core
 
 
+formatter = logging.Formatter('--> %(levelname)s'
+                                ': %(name)s'
+                                ': %(module)s'
+                                ': %(funcName)s'
+                                ': %(lineno)s'
+                                ' --> %(message)s')
+log = logging.getLogger(__name__)
+log.handlers = []
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+log.setLevel(logging.INFO)
+log.addHandler(ch)
+
+
+
+
 def get_camera():
-    panel = pymel.core.getPanel(withFocus=True)
-    camera = 'cam1'
+    active_pane = pymel.core.getPanel(withFocus=True)
+    camera = pymel.core.modelPanel(active_pane, camera=True, query=True)
+
+    log.debug(active_pane)
+    log.debug(type(active_pane))
+    log.debug(camera)
 
     return camera
 
