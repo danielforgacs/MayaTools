73759909c403a65a69b83f10ded7e6d8121a41c7
forgacs.daniel <forgacs.daniel@gmail.com>
2016-01-25 23:19:30 +0000

    camstabilizer update; gets camera properly; added exception handling

--- CamStabilizer/camstabilizer.py
+++ CamStabilizer/camstabilizer.py
@@ -1,11 +1,23 @@
+"""
+camera viewport stabilizer
+for Maya for matchmovers
+
+refactored code of stabilizer / fstab
+
+select one anything with transform.
+geo, locator, geo components...
+activate pane with camera
+or add a camera to the selection
+"""
+
 import logging
 
 import pymel.core
 
 
 formatter = logging.Formatter('--> %(levelname)s'
-                                ': %(name)s'
-                                ': %(module)s'
+                                # ': %(name)s'
+                                # ': %(module)s'
                                 ': %(funcName)s'
                                 ': %(lineno)s'
                                 ' --> %(message)s')
@@ -14,6 +26,7 @@ log.handlers = []
 ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 log.setLevel(logging.INFO)
+# log.setLevel(logging.DEBUG)
 log.addHandler(ch)
 
 
@@ -21,17 +34,47 @@ log.addHandler(ch)
 
 def get_camera():
     active_pane = pymel.core.getPanel(withFocus=True)
-    camera = pymel.core.modelPanel(active_pane, camera=True, query=True)
+    selection = pymel.core.selected()
+    camera = None
+    camera_shape = None
 
-    log.debug(active_pane)
-    log.debug(type(active_pane))
+    if len(selection) == 1:
+        try:
+            camera = pymel.core.modelPanel(active_pane, camera=True, query=True)
+        except:
+            pass
+
+    if not camera and (len(selection) == 2):
+        try:
+            camera = selection[1]
+        except:
+            pass
+
+    if camera:
+        try:
+            camera_shape = pymel.core.nt.Transform(camera).getShape()
+        except:
+            log.debug('cam shape error')
+            pass
+
+    if not isinstance(camera_shape, pymel.core.nodetypes.Camera):
+        camera_shape = None
+
+    if not camera_shape:
+            pymel.core.warning('--> Select a transform and a camera or click in a pane and select a transform')
+
+            return None
+
     log.debug(camera)
+    log.debug(camera_shape)
 
     return camera
 
 
 def stabilize():
     camera = get_camera()
+
+    return [camera]
 
 
 def main(**kwargs):
