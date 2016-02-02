5741179336ca8d602541c9d11a9b00be9109ceb0
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-02 01:41:10 +0000

    constrain loc to vertex tests update; works in terminal in mayapy

--- utils/constrainLoc2vtx_tests.py
+++ utils/constrainLoc2vtx_tests.py
@@ -1,6 +1,31 @@
+"""
+Tests for Constrain Locator To Vertex module
+
+offline tests run in mayapy in a terminal
+skipping tests requireing GUI
+
+for offline testing run:
+mayapy constrainLoc2vtx_tests.py
+"""
+
+
 import os
 from os.path import abspath, dirname
 import unittest
+
+
+gui = False
+
+
+if __name__ != '__main__':
+    """
+    if the module is run as a script
+    in a terminal window in mayapy
+    pymel.core import is skipped
+    """
+    import pymel.core
+    gui = True
+
 
 import constrainLoc2vtx
 reload(constrainLoc2vtx)
@@ -10,9 +35,14 @@ class ContrsainLoc2vtxUnittestOffline(unittest.TestCase):
     def test__test_is_running(self):
         self.assertTrue(True)
 
+    # @unittest.skip('--> Passed...')
+    @unittest.skipIf(not gui, '--> Not in Maya GUI...')
+    def test__skip_if_no_GUI(self):
+        self.assertTrue(True)
+
 
 def run():
-    unittest.main(module=__name__, exit=False)
+    unittest.main(module=__name__, exit=False, verbosity=2)
 
 
 if __name__ == '__main__':
