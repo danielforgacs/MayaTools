466cc5cb26456a82d634cc7505873b0d030791b9
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-03 03:39:02 +0000

    constrain loc to vtx update; pass functional test

--- utils/constrainLoc2vtx.py
+++ utils/constrainLoc2vtx.py
@@ -1,12 +1,12 @@
 #! python2
 
 __author__		= 'forgacs.daniel@gmail.com'
-__version__		= 'v1.0'
 
 
-'''
-create and constrain a locator to a vertex with an expression
-'''
+"""
+create and constrain a locator
+to a vertex with an expression
+"""
 
 
 import maya.cmds as cmds
@@ -21,7 +21,7 @@ def	constrain_locator_to_vertex	():
 	#locator		= cmds.pickWalk (direction = 'up')[0]
 	cmds.expression	(name = locator, string	='''
 float $BBoxSize = '''	+	geo	+	'''.boundingBoxMinX;\n
-$vertexWorldPos[3] = `pointPosition -world '''		+	vertex	+	'''`;\n'''	+
+$vertexWorldPos = `pointPosition -world '''		+	vertex	+	'''`;\n'''	+
 locator	+	'''.translateX = $vertexWorldPos[0];\n'''	+
 locator	+	'''.translateY = $vertexWorldPos[1];\n'''	+
 locator	+	'''.translateZ = $vertexWorldPos[2];'''	)
