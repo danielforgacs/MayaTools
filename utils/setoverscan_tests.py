f013d67b8380526ba3f31ca53a365d78ed12d5f3
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-07 21:46:32 +0000

    set overscan tests update

--- utils/setoverscan_tests.py
+++ utils/setoverscan_tests.py
@@ -1,5 +1,5 @@
 import unittest
-import fractions
+from fractions import Fraction
 import setoverscan
 
 try:
@@ -17,21 +17,31 @@ except:
 
 class SetOverscanFuncTests(unittest.TestCase):
     def test__setoverscan(self):
-        old_res_x = 1920
-        old_res_y = 1080
-        osc_left = 10
+        res_x = 1000
+        res_y = 500
+        osc_left = 50
+        res_x_new = 1200
+        res_y_new = 600
+        image_ratio = Fraction(res_x, res_y)
+        # res_x_new = res_x+(osc_left*2)
+        # res_y_new = float(res_x_new / image_ratio)
+
+        print('image_ratio: ', float(image_ratio))
+        print('overscan left / right: ', osc_left*2)
+        print('res_x: ', res_x)
+        print('res_x_new: ', res_x_new)
+        print('res_y: ', res_y)
+        print('res_y_new: ', res_y_new)
 
         pymel.core.newFile(force=True)
 
         rendersettings = pymel.core.PyNode('defaultResolution')
-        rendersettings.setAttr('width', old_res_x)
-        rendersettings.setAttr('height', old_res_y)
+        rendersettings.setAttr('width', res_x)
+        rendersettings.setAttr('height', res_y)
 
-        imageaspect = fractions.Fraction(old_res_x, old_res_y)
-
-        self.assertIsNone(setoverscan.main())
-        self.assertEqual(rendersettings.getAttr('width'), 1920)
-        self.assertEqual(rendersettings.getAttr('height'), 1080)
+        self.assertIsNone(setoverscan.main(pixels=osc_left))
+        self.assertEqual(rendersettings.getAttr('width'), res_x_new)
+        self.assertEqual(rendersettings.getAttr('height'), res_y_new)
 
 
 
@@ -39,5 +49,5 @@ def main():
     unittest.main(module=__name__, exit=False)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__' and pymel:
     unittest.main(module=__name__, exit=False)
