76cdc4ecf469b2e64d77f9795807f8998d915171
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-07 21:02:23 +0000

    set overscan updated

--- utils/setoverscan.py
+++ utils/setoverscan.py
@@ -1,6 +1,27 @@
 """
 calculate and set camera values
 for overscan in one camera setup
+
+overscan is not uniform. It matches
+image proportions
+
+default overscan:
+    10 / 10 pixels :: left / right
+
+
+###
+w = 1920.0
+h = 1440.0
+w_osc = 1980
+w_extra = w_osc - w
+osc_factor = w / w_osc
+h_osc = h / osc_factor
+
+print '\\noriginal res: ', w, '*', h
+print 'overscan res', w_osc, '*', h_osc
+print 'overscan factor (uniform)', osc_factor
+print 'extra pixels (width, height)', w_osc - w, h_osc - h
+###
 """
 
 
@@ -10,7 +31,10 @@ def set_camera_post_scale(ratio):
     return postscale
 
 
-def set_overscan(pixels=20):
+def set_overscan(pixels=10):
+    pass
+
+def main():
     pass
 
 
