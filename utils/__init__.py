2c7e328a7a120a70abbfdb36ef0c648f094e17e4
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-07 19:36:17 +0000

    added set overscan to maya tools init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,12 @@
 from . import constrainLoc2vtx
+from . import setoverscan
 
 
 try:
     reload(constrainLoc2vtx)
+    reload(setoverscan)
 except:
     import importlib
 
     importlib.reload(constrainLoc2vtx)
+    importlib.reload(setoverscan)
