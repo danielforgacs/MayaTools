c18ca496c1acade121715544cf6bf264e511ae24
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-07 21:46:20 +0000

    set overscan update

--- utils/setoverscan.py
+++ utils/setoverscan.py
@@ -25,17 +25,34 @@ print 'extra pixels (width, height)', w_osc - w, h_osc - h
 """
 
 
+from fractions import Fraction
+import pymel.core
+
+
 def set_camera_post_scale(ratio):
     postscale = None
 
     return postscale
 
 
-def set_overscan(pixels=10):
-    pass
+def set_osc_resolution(pixels):
+    rendersettings = pymel.core.PyNode('defaultResolution')
+    res_x = rendersettings.getAttr('width')
+    res_y = rendersettings.getAttr('height')
+    image_ratio = Fraction(res_x, res_y)
+    res_y_new = res_y+(pixels*2)
+    res_x_new = float(res_y_new * image_ratio)
 
-def main():
-    pass
+    rendersettings.setAttr('width', res_x_new)
+    rendersettings.setAttr('height', res_y_new)
+
+    return (res_x_new, res_y_new)
+
+
+
+def main(**kwargs):
+    set_osc_resolution(pixels=kwargs.get('pixels', 10))
+
 
 
 def test():
