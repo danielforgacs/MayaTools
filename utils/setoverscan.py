10c1029146d31d02b52113b0903d9dbd07a69c65
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-07 19:36:48 +0000

    set oscan update

--- utils/setoverscan.py
+++ utils/setoverscan.py
@@ -1,4 +1,27 @@
 """
 calculate and set camera values
-for overscane in one camera setup
+for overscan in one camera setup
 """
+
+
+def set_camera_post_scale(ratio):
+    postscale = None
+
+    return postscale
+
+
+def set_overscan(pixels=20):
+    pass
+
+
+def test():
+    import setoverscan_tests
+
+    try:
+        reload(setoverscan_tests)
+    except:
+        import importlib
+
+        importlib.reload(setoverscan_tests)
+
+    setoverscan_tests.main()
