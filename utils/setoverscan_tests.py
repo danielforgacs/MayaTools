f527b586d875e6c76397618e0701324c594ab51f
forgacs.daniel <forgacs.daniel@gmail.com>
2016-02-07 21:02:34 +0000

    set overscan tests updated

--- utils/setoverscan_tests.py
+++ utils/setoverscan_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import fractions
 import setoverscan
 
 try:
@@ -8,13 +9,35 @@ except:
     importlib.reload(setoverscan)
 
 
-class SetOverscanTests(unittest.TestCase):
-    def setUp(self):
-        pass
+try:
+    import pymel.core
+except:
+    pymel = None
 
-    def test__setoverscane_tests_are_running(self):
-        self.assertTrue(True)
+
+class SetOverscanFuncTests(unittest.TestCase):
+    def test__setoverscan(self):
+        old_res_x = 1920
+        old_res_y = 1080
+        osc_left = 10
+
+        pymel.core.newFile(force=True)
+
+        rendersettings = pymel.core.PyNode('defaultResolution')
+        rendersettings.setAttr('width', old_res_x)
+        rendersettings.setAttr('height', old_res_y)
+
+        imageaspect = fractions.Fraction(old_res_x, old_res_y)
+
+        self.assertIsNone(setoverscan.main())
+        self.assertEqual(rendersettings.getAttr('width'), 1920)
+        self.assertEqual(rendersettings.getAttr('height'), 1080)
+
 
 
 def main():
+    unittest.main(module=__name__, exit=False)
+
+
+if __name__ == '__main__':
     unittest.main(module=__name__, exit=False)
