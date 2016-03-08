35892733b44a293af83f794c5dfd57b904a5e281
forgacs.daniel <forgacs.daniel@gmail.com>
2016-03-08 23:35:04 +0000

    started world space nake tool

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,7 +1,9 @@
 from . import camstabilizer
 from . import constrainLoc2vtx
 from . import setoverscan
+from . import worldSpaceBake
 
 reload(camstabilizer)
 reload(constrainLoc2vtx)
 reload(setoverscan)
+reload(worldSpaceBake)
---
+++ tools/worldSpaceBake.py
@@ -0,0 +1,8 @@
+try:
+    import pymel.core as pm
+except:
+    pm = None
+
+
+def main():
+    pass
