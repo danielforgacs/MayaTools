54e7170f8f2e369b266e440bf88ae5314f498e77
forgacs.daniel <forgacs.daniel@gmail.com>
2016-03-19 19:22:03 +0000

    deleted camstabilizer

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,9 +1,7 @@
-from . import camstabilizer
 from . import constrainLoc2vtx
 from . import setoverscan
 from . import worldSpaceBake
 
-reload(camstabilizer)
 reload(constrainLoc2vtx)
 reload(setoverscan)
 reload(worldSpaceBake)
