f5c7e7fac7c81cdc6b74db06dea17d9a49ff8063
forgacs.daniel <forgacs.daniel@gmail.com>
2016-03-19 19:50:45 +0000

    added camstab to init py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,7 +1,9 @@
 from . import constrainLoc2vtx
 from . import setoverscan
 from . import worldSpaceBake
+from . import camstab
 
 reload(constrainLoc2vtx)
 reload(setoverscan)
 reload(worldSpaceBake)
+reload(camstab)
