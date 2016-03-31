c3f2ed6ca3ddf78fe898a4495ae5bd47395cca64
danielf@Azure <dforgacs@ad.blue2p0.com@bluewk010-l.ad.blue2p0.com>
2016-03-31 12:04:21 +0100

    added atom updater

---
+++ tools/atomupdate.py
@@ -0,0 +1,39 @@
+import pymel.core as pm
+
+
+def get_selection(prefix=''):
+	sel = pm.ls(selection=True)
+	new_sel = [pm.PyNode(prefix + node.name()) for node in sel]
+
+	return (sel, new_sel)
+
+def export_atom(node):
+	pass
+
+
+def import_atom(node):
+	pass
+
+
+def export_all_anim(sel):
+	for node in sel:
+		export_atom(node)
+
+
+def import_all_anim(sel, new_sel):
+	for k, node in enumerate(sel):
+		import_atom(new_sel[k])
+
+
+
+def main():
+	sel, new_sel = get_selection('new_')
+
+	export_all_anim(sel)
+	import_all_anim(sel, new_sel)
+
+	pm.select(new_sel)
+
+
+if __name__ == '__main__':
+	main()
